a6024bad045ce2b34db50c97c635511db7087e51
SayadSindid <SayadSindid@proton.me>
2025-07-17 17:42:23 +0200

    fix missing civ (romans)

--- src/utility/data.ts
+++ src/utility/data.ts
@@ -1,78 +1,81 @@
-
 // List of civ
 // FIXME: Cange the data structure in order to put the emoji id for every civ
 // Probably an array of object
 export const civilizations = [
-    // The Age of Kings
-    "Britanniques",
-    "Byzantins",
-    "Celtes",
-    "Chinois",
-    "Francs",
-    "Goths",
-    "Japonais",
-    "Mongols",
-    "Perses",
-    "Sarrasins",
-    "Teutons",
-    "Turcs",
-    "Vikings",
+	// The Age of Kings
+	"Britanniques",
+	"Byzantins",
+	"Celtes",
+	"Chinois",
+	"Francs",
+	"Goths",
+	"Japonais",
+	"Mongols",
+	"Perses",
+	"Sarrasins",
+	"Teutons",
+	"Turcs",
+	"Vikings",
 
-    // The Conquerors
-    "Aztèques",
-    "Coréens",
-    "Espagnols",
-    "Huns",
-    "Mayas",
+	// The Conquerors
+	"Aztèques",
+	"Coréens",
+	"Espagnols",
+	"Huns",
+	"Mayas",
 
-    // The Forgotten
-    "Hongrois",
-    "Incas",
-    "Italiens",
-    "Slaves",
+	// The Forgotten
+	"Hongrois",
+	"Incas",
+	"Italiens",
+	"Slaves",
 
-    // The African Kingdoms
-    "Berbères",
-    "Éthiopiens",
-    "Maliens",
-    "Portugais",
+	// The African Kingdoms
+	"Berbères",
+	"Éthiopiens",
+	"Maliens",
+	"Portugais",
 
-    // Rise of the Rajas
-    "Birmans",
-    "Khmers",
-    "Malais",
-    "Vietnamiens",
+	// Rise of the Rajas
+	"Birmans",
+	"Khmers",
+	"Malais",
+	"Vietnamiens",
 
-    // The Last Khans
-    "Bulgares",
-    "Coumans",
-    "Lituaniens",
-    "Tatars",
+	// The Last Khans
+	"Bulgares",
+	"Coumans",
+	"Lituaniens",
+	"Tatars",
 
-    // Lords of the West
-    "Bourguignons",
-    "Siciliens",
+	// Lords of the West
+	"Bourguignons",
+	"Siciliens",
 
-    // Dawn of the Dukes
-    "Bohémiens",
-    "Polonais",
+	// Dawn of the Dukes
+	"Bohémiens",
+	"Polonais",
 
-    // Dynasties of India
-    "Bengalis",
-    "Dravidiens",
-    "Gurjaras",
-    "Hindoustanis",
+	// Dynasties of India
+	"Bengalis",
+	"Dravidiens",
+	"Gurjaras",
+	"Hindoustanis",
 
-    // The Mountain Royals
-    "Arméniens",
-    "Géorgiens",
+	// The Mountain Royals
+	"Arméniens",
+	"Géorgiens",
 
-    // The Three Kingdoms
-    "Jürchen",
-    "Khitan",
-    "Shu",
-    "Wei",
-    "Wu",
+	// The Three Kingdoms
+	"Jürchen",
+	"Khitan",
+	"Shu",
+	"Wei",
+	"Wu",
+
+	"Romains",
 ];
 
-export const sortedCivlizations = civilizations.sort((a, b) => a.localeCompare(b))
+export const sortedCivlizations = civilizations.sort((a, b) =>
+	a.localeCompare(b),
+);
